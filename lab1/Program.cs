4d9d4c90008f74e8cc2781e06438f0968b2cebf0
umar11b <uzaman2018@gmail.com>
2025-09-12 21:42:42 -0400

    Initial commit: Game Tools lab1 with vector addition demo

---
+++ lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Vector Addition Test");
+        Console.WriteLine("===================");
+        
+        AddVectors();
+        
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+    
+    private static void AddVectors()
+    {
+        Vector2 a = new Vector2(3, 5);
+        Vector2 b = new Vector2(2, -1);
+        Vector2 c = a + b;
+        Console.WriteLine($"Vector a {a} + b {b} = {c}");
+    }
+}
